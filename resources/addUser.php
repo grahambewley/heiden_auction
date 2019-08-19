5d4a9189a9dfd2baf659e1883c2a92f9700955bb
Graham Bewley <grahambewley@gmail.com>
2019-08-19 19:17:26 -0400

    Test SQL query

---
+++ resources/addUser.php
@@ -0,0 +1,11 @@
+<?php
+
+include('dbConnect.php');
+
+$sql = "INSERT INTO users (name, secret) VALUES ('awagner', 'heidenRulez')";
+if (mysqli_query($conn, $sql)) {
+      echo "New user created successfully";
+} else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+mysqli_close($conn);
