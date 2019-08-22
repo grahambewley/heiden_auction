a59cc7f4afb131737dfeb5eb5c8491cfde9b98a9
Graham Bewley <grahambewley@gmail.com>
2019-08-22 07:36:07 -0400

    Add high_bid_id to item

--- resources/addBidToBids.php
+++ resources/addBidToBids.php
@@ -3,8 +3,13 @@
 // Include a file hidden from Github that contains database connection settings and connects to the database
 include('dbConnect.php');
 
-// SQL query for largest (latest) start_date_time in auctions table 
-$result = mysqli_query($conn, 
-"INSERT INTO bids (item_id, user_id, amount) VALUES(".$_REQUEST['biddingItemId'].", ".$_REQUEST['biddingUserId'].", ".$_REQUEST['biddingValue']."); ");
+// SQL query for Inserting bid into bids table 
+mysqli_query($conn, "INSERT INTO bids (item_id, user_id, amount) VALUES(".$_REQUEST['biddingItemId'].", ".$_REQUEST['biddingUserId'].", ".$_REQUEST['biddingValue']."); ");
 
-echo mysqli_insert_id($conn);
+// mysqui_insert_id gets the id created in the last query
+$bid_id = mysqli_insert_id($conn);
+
+// SQL query for updating item to reflect new high_bid_id
+mysqli_query($conn, "UPDATE items SET high_bid_id = '".$bid_id."' WHERE id = ".$_REQUEST['biddingItemId']);
+
+echo $bid_id;
