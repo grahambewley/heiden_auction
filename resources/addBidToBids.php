8a50526b7d287efab6ced87d3ea96cfed74c9ebc
Graham Bewley <grahambewley@gmail.com>
2019-08-22 06:49:08 -0400

    Tweak bid php

--- resources/addBidToBids.php
+++ resources/addBidToBids.php
@@ -4,14 +4,7 @@
 include('dbConnect.php');
 
 // SQL query for largest (latest) start_date_time in auctions table 
-$result = mysqli_query($conn, "INSERT INTO bids (item_id, user_id, amount) OUTPUT Inserted.id VALUES(".$_REQUEST['biddingItemId'].", ".$_REQUEST['biddingUserId'].", ".$_REQUEST['biddingValue'].");");
+$result = mysqli_query($conn, "INSERT INTO bids (item_id, user_id, amount) VALUES(".$_REQUEST['biddingItemId'].", ".$_REQUEST['biddingUserId'].", ".$_REQUEST['biddingValue'].");");
 
-if ($result->num_rows > 0) {
-
-    while($row = $result->fetch_assoc()) {
-        $jsonResult = json_encode($row);
-        echo $jsonResult;
-    }
-} else {
-    echo "0 results";
-}
+$last_id = $conn->insert_id;
+echo $last_id;
