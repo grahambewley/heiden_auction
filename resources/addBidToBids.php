7b437f41d7c1bf370764210ac52e641d9d32c6d6
Graham Bewley <grahambewley@gmail.com>
2019-08-22 07:22:00 -0400

    Switch to echo mysqli insert_id

--- resources/addBidToBids.php
+++ resources/addBidToBids.php
@@ -4,11 +4,7 @@
 include('dbConnect.php');
 
 // SQL query for largest (latest) start_date_time in auctions table 
-$result = mysqli_query($conn, "INSERT INTO bids (item_id, user_id, amount) VALUES(".$_REQUEST['biddingItemId'].", ".$_REQUEST['biddingUserId'].", ".$_REQUEST['biddingValue'].");");
+$result = mysqli_query($conn, 
+"INSERT INTO bids (item_id, user_id, amount) VALUES(".$_REQUEST['biddingItemId'].", ".$_REQUEST['biddingUserId'].", ".$_REQUEST['biddingValue']."); ");
 
-if ($conn->query($sql) === TRUE) {
-    $last_id = $conn->insert_id;
-    echo "New bid created successfully. Last inserted bid's ID is: " . $last_id;
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
+echo $mysqli->insert_id;
