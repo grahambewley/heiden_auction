d698af2dea9a43094e7cfa1ef21fbceafae884bb
Graham Bewley <grahambewley@gmail.com>
2019-08-22 06:53:03 -0400

    Add if statement for connection failures to bid PHP

--- resources/addBidToBids.php
+++ resources/addBidToBids.php
@@ -6,5 +6,9 @@ include('dbConnect.php');
 // SQL query for largest (latest) start_date_time in auctions table 
 $result = mysqli_query($conn, "INSERT INTO bids (item_id, user_id, amount) VALUES(".$_REQUEST['biddingItemId'].", ".$_REQUEST['biddingUserId'].", ".$_REQUEST['biddingValue'].");");
 
-$last_id = $conn->insert_id;
-echo $last_id;
+if ($conn->query($sql) === TRUE) {
+    $last_id = $conn->insert_id;
+    echo "New bid created successfully. Last inserted bid's ID is: " . $last_id;
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
