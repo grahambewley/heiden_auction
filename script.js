6e3f2e0f3a91248da10272481e53044f2a98735c
Graham Bewley <grahambewley@gmail.com>
2019-08-25 10:57:38 -0400

    Fix auction time banners

--- script.js
+++ script.js
@@ -65,10 +65,8 @@ getCurrentAuctionMetadata = function () {
         //Determine if this auction has ended or not started, if so then display a banner, hide bid buttons
         if(currentEpochTime > selectedAuctionEndUtcSeconds) {
             document.getElementById('auction-ended-banner').style.display = "block";
-            document.getElementById('auction-ended-banner__text').innerHTML = "&#9888; This auction has ended.";
         } else if (currentEpochTime <= selectedAuctionStartUtcSeconds) {
-            document.getElementById('auction-ended-banner').style.display = "block";
-            document.getElementById('auction-ended-banner__text').innerHTML = "&#9888; This auction has not yet started."
+            document.getElementById('auction-not-started-banner').style.display = "block";
         }
 
         //Once we've got the correct auction selected we can fill in the auction items
