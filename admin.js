72baa9b907959e00fbe3f27b971c617d0e7c6517
Graham Bewley <grahambewley@gmail.com>
2019-08-26 22:17:13 -0400

    Admin page pull auctions table

--- admin.js
+++ admin.js
@@ -14,7 +14,23 @@ function loadAdminTableData() {
 
         let resultArray = JSON.parse(result);
 
+        // For each auction...
+        resultArray.forEach(function (element) {
         
+            let auctionRow = document.createElement('tr');
+
+            let auctionId = element.id;
+            let auctionName = element.name;
+            let auctionStart = moment.unix(element.start_date_time).format('M/D/YY h:mm A');
+            let auctionEnd = moment.unix(element.end_date_time).format('M/D/YY h:mm A');
+
+            auctionRow.appendChild(document.createElement('td').innerHTML = auctionId);
+            auctionRow.appendChild(document.createElement('td').innerHTML = auctionName);
+            auctionRow.appendChild(document.createElement('td').innerHTML = auctionStart);
+            auctionRow.appendChild(document.createElement('td').innerHTML = auctionEnd);
+        
+            
+        });
 
     });
 
