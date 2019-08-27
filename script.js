4dd2d6b3930016e7d7dff9c97e50f25bf98b6a7a
Stozzy <sagoldyn@gmail.com>
2019-08-26 22:15:42 -0400

    created seperate function for log off button - moved localstorage clear and window.location to this function.

--- script.js
+++ script.js
@@ -39,11 +39,14 @@ displayUserName = function () {
 
         let logOff = document.createElement('button');
         logOff.setAttribute('class', 'btn btn--small');
-        logOff.setAttribute('onclick', '(function() {
+        logOff.setAttribute('onclick', 'logOffUser()');
+        logOff.innerHTML = "Log Off";
+
+        function logOffUser() {
             localStorage.clear();
             window.location.href = "/auction/login.html";
-        })())=;'
-        logOff.innerHTML = "Log Off";
+        };
+
 
         userInfo.appendChild(welcomeText);
         userInfo.appendChild(viewBids);
