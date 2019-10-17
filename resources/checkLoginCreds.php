3578032687918818e6dfb30cb4654c99cf56e2ba
sagoldyn@gmail.com <3kGu*v2ivLYz1hKg>
2019-10-17 05:59:50 -0400

    The only thing I changed here (exception of comments) was add code for obtaining POST data.  Get an error 500 when login.html is submitted

--- resources/checkLoginCreds.php
+++ resources/checkLoginCreds.php
@@ -3,19 +3,36 @@
 // Include a file hidden from Github that contains database connection settings and connects to the database
 include('dbConnect.php');
 
-// Get variables passed over from JavaScript file
-$username = $_REQUEST['user'];
-$password = $_REQUEST['pw'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  	$username = $_POST["usr"];
+	$password = $_POST["pw"];
 
-$result = mysqli_query($conn, "SELECT * from users WHERE user='".$username."' AND BINARY pw='".$password."'");
+	$result = mysqli_query($conn, "SELECT * from users WHERE user='".$username."' AND BINARY pw='".$password."'");
 
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        $jsonResult = json_encode($row);
-        echo $jsonResult;
-    }
-} else {
-    echo 0;
+	if ($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+			$jsonResult = json_encode($row);
+			echo $jsonResult;
+		}
+	} else {
+		echo 0;
 }
 
 $conn->close();
+
+//// Get variables passed over from JavaScript file
+//$username = $_REQUEST['user'];
+//$password = $_REQUEST['pw'];
+//
+//$result = mysqli_query($conn, "SELECT * from users WHERE user='".$username."' AND BINARY pw='".$password."'");
+//
+//if ($result->num_rows > 0) {
+//	while($row = $result->fetch_assoc()) {
+//		$jsonResult = json_encode($row);
+//		echo $jsonResult;
+//	}
+//} else {
+//	echo 0;
+//}
+
+//	$conn->close();
